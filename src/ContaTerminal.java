9b8c019ac6196c3b45a9bd64e99294b769fb8b69
GiovannaOliveira18 <giovanna.oliveira1@unesp.br>
2025-07-13 16:30:49 -0300

    Desafio completo

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,41 @@
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e Importar classe Scanner
+        Scanner valor = new Scanner(System.in);
+        
+        //Exibir Mensagens para o usuário
+        System.out.println("Bem Vindo ao seu Banco Digital!!\n");
+
+        //Obter pela classe Scanner os valores digitados
+        System.out.println("Digite o número:");
+        int numeroConta = valor.nextInt();
+        valor.nextLine(); 
+
+        System.out.println("Digite o número da Agência:");
+        String numeroAgencia = valor.nextLine();
+
+        System.out.println("Digite o seu nome:");
+        String nomeCliente = valor.nextLine();
+
+        System.out.print("Informe o saldo inicial (valor decimal): ");
+        BigDecimal saldo = valor.nextBigDecimal();
+        
+        System.out.println("\nDados informados:");
+        
+        System.out.println("Número da conta digitado: " + numeroConta);
+        System.out.println("Número da agência digitado: " + numeroAgencia);
+        System.out.println("Nome do cliente digitado: " + nomeCliente);
+        
+        System.out.printf("Saldo formatado: R$ %.2f\n", saldo.doubleValue());
+        //Exibir Mensagem Final = "Conta Criada"
+        System.out.println("\nConta criada com sucesso!");
+
+        valor.close();
+        
+
     }
 }
